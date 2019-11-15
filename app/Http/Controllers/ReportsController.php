7a89c8708bbde1ae3fa916face0cb0589ea86591
ite19-3c-19@M303.uc-bcf.edu.ph <ite19-3c-19@M303.uc-bcf.edu.ph>
2019-11-15 11:51:52 +0800

    first

---
+++ app/Http/Controllers/ReportsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+use PhpOffice\PhpWord\TemplateProcessor;
+use Illuminate\Http\Request;
+
+class ReportsController extends Controller
+{
+    public function word()
+    {
+    	$templateProcessor = new TemplateProcessor('./templates/Certificate of Recognition.docx');
+
+    	$templateProcessor->setValue('first_name', 'Juan');
+    	$templateProcessor->setValue('last_name', 'Blank');
+
+    	$templateProcessor->saveAs('Juan DelaCruz.docx');
+
+    	return response()->download('Juan DelaCruz.docx');
+    }
+
+      public function excel()
+    {
+    	$spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load('./templates/form138.xlsx');
+
+		$worksheet = $spreadsheet->getActiveSheet();
+
+		$worksheet->getCell('A7')->setValue('Name: Juan dela');
+		$worksheet->getCell('A8')->setValue('11-B');
+
+		$writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($spreadsheet, 'Xls');
+		$writer->save('form138.xlsx');
+
+		return response()->download('form138.xlsx');
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.master')
+@section('content')
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+   Inventory
+  </a>
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" >Primary</button>
+
+<div class="modal" tabindex="-1" id="exampleModal" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+</nav>
+
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+</table>
+
+@endsection
